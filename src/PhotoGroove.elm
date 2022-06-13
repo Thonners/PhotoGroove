f2456ce281e4dff1c2a4a2d0b539957694e834b2
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2022-06-13 22:35:04 +0100

    Chapter 2

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,17 +1,54 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1, img, text)
+import Browser
+import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
-view model = 
+urlPrefix =
+    "http://elm-in-action.com/"
+
+view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+        , div [ id "thumbnails" ] 
+            (List.map 
+                (viewThumbnail model.selectedURL)
+                model.photos
+            ) 
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedURL)
             ]
+            []
         ]
 
-main = 
-    view "no model yet"
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ("selected", selectedUrl == thumb.url)]
+        , onClick {description = "ClickedPhoto", data = thumb.url }
+        ]
+        []
+
+initialModel =
+    { photos = 
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedURL = "1.jpeg"
+    }
+
+update msg model =
+    if msg.description == "ClickedPhoto" then
+       { model | selectedURL = msg.data }
+    else
+        model
+
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
