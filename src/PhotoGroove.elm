4a370dd367ee8868c3a05265d0a0ce8646e92de8
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2022-06-25 11:22:28 +0100

    Chapter 3.2.1

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,21 +1,45 @@
 module PhotoGroove exposing (main)
 
+import Array exposing (Array)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
+
+urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
+
+type alias Model =
+    { photos : List Photo
+    , selectedURL : String
+    }
+
+
+type alias Photo =
+    { url : String }
+
+
+type alias Msg =
+    { description : String
+    , data : String
+    }
+
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ] 
-            (List.map 
+        , button
+            [ onClick { description = "ClickedSurpriseMe", data = "" } ]
+            [ text "Surprise Me!" ]
+        , div [ id "thumbnails" ]
+            (List.map
                 (viewThumbnail model.selectedURL)
                 model.photos
-            ) 
+            )
         , img
             [ class "large"
             , src (urlPrefix ++ "large/" ++ model.selectedURL)
@@ -23,16 +47,20 @@ view model =
             []
         ]
 
+
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
-        , classList [ ("selected", selectedUrl == thumb.url)]
-        , onClick {description = "ClickedPhoto", data = thumb.url }
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url }
         ]
         []
 
+
+initialModel : Model
 initialModel =
-    { photos = 
+    { photos =
         [ { url = "1.jpeg" }
         , { url = "2.jpeg" }
         , { url = "3.jpeg" }
@@ -40,11 +68,24 @@ initialModel =
     , selectedURL = "1.jpeg"
     }
 
+
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+update : Msg -> Model -> Model
 update msg model =
-    if msg.description == "ClickedPhoto" then
-       { model | selectedURL = msg.data }
-    else
-        model
+    case msg.description of
+        "ClickedPhoto" ->
+            { model | selectedURL = msg.data }
+
+        "ClickedSurpriseMe" ->
+            { model | selectedURL = "2.jpeg" }
+
+        _ ->
+            model
+
 
 main =
     Browser.sandbox
